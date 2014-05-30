2e1f6d7519002b48dbe4d4d0134d27349a249ec5
gerritjvv <gerritjvv@gmail.com>
2014-05-30 16:09:20 +0200

    test init exec stop terminate

--- test/thread_load/core_test.clj
+++ test/thread_load/core_test.clj
@@ -4,7 +4,9 @@
     [clojure.test.check.generators :as gen]
     [clojure.test.check.properties :as prop]
     [clojure.test.check.clojure-test :refer [defspec]]
-    [thread-load.core :refer :all]))
+    [thread-load.core :refer :all])
+  
+  (:import [java.util.concurrent ArrayBlockingQueue]))
 
 
 (defspec call-f-should-return-fail-on-exception
@@ -29,4 +31,23 @@
                   (fn [& _] {:called [:init] :status :fail}) (fn [&_]) 
                   (fn [{:keys [called status]} data]  
                     {:called (conj called :stop) :status :terminate}))]
-        (= [:init :stop] (:called state)))))
+        (= [:init :stop] (:called state)))))\
+                                             
+
+
+
+
+(defspec worker-runner-should-call-init-exec-stop-and-terminate
+  10
+  (prop/for-all [a gen/nat]
+    (let [queue (doto (ArrayBlockingQueue. 10) (.add :a))
+          state (woker-runner! queue 
+                  (fn [& _] {:called [:init] }) 
+                  (fn [{:keys [called status]} data]
+                    {:called (conj called :exec) :data data :status :fail})
+                  (fn [{:keys [called status] :as state} data]  
+                    (assoc state :called (conj called :stop) :status :terminate)))]
+        (and 
+          (= [:init :exec :stop] (:called state))
+          (= :a (:data state))))))
+
