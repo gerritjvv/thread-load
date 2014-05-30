f704839d55a7cacecb0a6eba07d08cf1611cb3f5
gerritjvv <gerritjvv@gmail.com>
2014-05-30 11:09:42 +0200

    update project

--- project.clj
+++ project.clj
@@ -3,4 +3,17 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  :global-vars {*warn-on-reflection* true
+                *assert* false}
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
+  :java-source-paths ["java"]
+  
+  :plugins [
+         [lein-midje "3.0.1"] [lein-marginalia "0.7.1"]
+         [lein-kibit "0.0.8"] [no-man-is-an-island/lein-eclipse "2.0.0"]
+           ]
+  :dependencies [
+		[org.clojure/tools.logging "0.2.6"]
+                [midje "1.6-alpha2" :scope "test"]
+		[org.clojure/clojure "1.6.0"]])
