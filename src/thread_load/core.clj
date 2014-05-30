1eec21b78d5044ee6f26e065682357c43b35b31d
gerritjvv <gerritjvv@gmail.com>
2014-05-30 16:34:09 +0200

    fix bugs and test publish read

--- src/thread_load/core.clj
+++ src/thread_load/core.clj
@@ -1,6 +1,6 @@
 (ns thread-load.core
-  
-  (:import [java.util.concurrent BlockingQueue ArrayBlockingQueue Executors ExecutorService]))
+  (:require [clojure.tools.logging :refer [error]])
+  (:import [java.util.concurrent BlockingQueue ArrayBlockingQueue Executors ExecutorService TimeUnit]))
 
 
 (defn call-f 
@@ -8,9 +8,11 @@
   ([f state]
     (call-f f state nil))
   ([f state data]
+    (call-f f state data false))
+  ([f state data terminate?]
   (try
     (f state data)
-    (catch Throwable t (assoc state :status :fail :throwable t)))))
+    (catch Throwable t (assoc state :status (if terminate? :terminate :fail) :throwable t)))))
 
 (defn get-queue-data! 
   "Blocks till data is available on the queue and returns the data, queue must be BlockingQueue"
@@ -20,7 +22,7 @@
 (defn call-on-fail 
   "Only calls init if the return of stop is not :terminate of :fail"
   [init stop state]
-  (let [new-state (call-f stop state)
+  (let [new-state (call-f stop state nil true) ;if stop throws an exception we need to terminate
         status (:status new-state)]
     (if (not (or (= status :terminate) (= status :fail)))
       (call-f init new-state)
@@ -31,7 +33,7 @@
   [queue exec state]
   (call-f exec state (get-queue-data! queue)))
 
-(defn woker-runner! 
+(defn worker-runner! 
   "This function only returns if any of the init, exec, stop functions return status :terminate in the state map"
   [queue init exec stop]
    (loop [state (call-f init {})] 
@@ -46,14 +48,30 @@
    })
 
 (defn add-consumer 
-  "Adds a consumer function tri to the thread pool, which will take data of the queue and run exec on the data"
+  "Adds a consumer function tri to the thread pool, which will take data of the queue and run exec on the data
+   Returns the pool"
   [pool init exec stop]
   (let [^ExecutorService thread-pool (:thread-pool pool)
-        ^Runnable r-f woker-runner!]
-    (.submit thread-pool r-f)))
+        ^Runnable r-f (fn []
+                        (try
+                          (worker-runner! (:queue pool) init exec stop)
+                          (catch Throwable t (prn t))))]
+    (.submit thread-pool r-f)
+    pool))
 
 (defn publish! 
   "Will block if the queue is full otherwise will send data to the queue which will be processed,
-   by one of the consumer functions"
+   by one of the consumer functions
+   Returns the pool"
   [pool data]
-   (.put ^BlockingQueue (:queue pool) data))
+   (.put ^BlockingQueue (:queue pool) data)
+   pool)
+
+(defn shutdown-pool 
+  "Shutdown the ExecutorService used and wait for termination timeout milliseconds, if the threads did not all terminate, the pool is shutdown forcefully"
+  [{:keys [^ExecutorService thread-pool]} timeout]
+  (.shutdown thread-pool)
+  (if (not (.awaitTermination thread-pool (long timeout) TimeUnit/MILLISECONDS))
+    (.shutdownNow thread-pool)))
+
+  
--- test/thread_load/core_test.clj
+++ test/thread_load/core_test.clj
@@ -6,7 +6,9 @@
     [clojure.test.check.clojure-test :refer [defspec]]
     [thread-load.core :refer :all])
   
-  (:import [java.util.concurrent ArrayBlockingQueue]))
+  (:import 
+    [java.util.concurrent ArrayBlockingQueue]
+    [java.util.concurrent.atomic AtomicInteger]))
 
 
 (defspec call-f-should-return-fail-on-exception
@@ -19,7 +21,7 @@
 (defspec worker-runner-should-call-init-and-terminate
   10
   (prop/for-all [a gen/nat]
-    (let [state (woker-runner! nil (fn [& _] {:called true :status :terminate}) (fn [&_]) (fn [&_]))]
+    (let [state (worker-runner! nil (fn [& _] {:called true :status :terminate}) (fn [&_]) (fn [&_]))]
       (:called state))))
 
 
@@ -27,7 +29,7 @@
 (defspec worker-runner-should-call-init-stop-and-terminate
   10
   (prop/for-all [a gen/nat]
-    (let [state (woker-runner! nil 
+    (let [state (worker-runner! nil 
                   (fn [& _] {:called [:init] :status :fail}) (fn [&_]) 
                   (fn [{:keys [called status]} data]  
                     {:called (conj called :stop) :status :terminate}))]
@@ -41,7 +43,7 @@
   10
   (prop/for-all [a gen/nat]
     (let [queue (doto (ArrayBlockingQueue. 10) (.add :a))
-          state (woker-runner! queue 
+          state (worker-runner! queue 
                   (fn [& _] {:called [:init] }) 
                   (fn [{:keys [called status]} data]
                     {:called (conj called :exec) :data data :status :fail})
@@ -51,3 +53,29 @@
           (= [:init :exec :stop] (:called state))
           (= :a (:data state))))))
 
+
+
+
+(defspec consume-from-pool 
+  10
+  (prop/for-all [a gen/nat]
+    (let [init-count (AtomicInteger.)
+          exec-count (AtomicInteger.)
+          stop-count (AtomicInteger.)
+          
+          pool (->
+                 (create-pool) 
+                 (add-consumer 
+                   (fn [& _] (.incrementAndGet init-count) {}) 
+                   (fn [& _] (.incrementAndGet exec-count) {:status :fail})
+                   (fn [& _] (.incrementAndGet stop-count) {:status :terminate}))
+                 (publish! :a)
+                 (shutdown-pool 10000))
+          ]
+      (prn init-count " " exec-count " " stop-count)
+      (and 
+        (= (.get init-count) 1)
+        (= (.get exec-count) 1)
+        (= (.get stop-count) 1)))))
+        
+                   
