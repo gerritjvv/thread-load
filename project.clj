1aba6c81ce5f6bc67260d5a2d1f1877466c015c3
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2017-02-03 09:02:40 +0100

    up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thread-load "0.3.2"
+(defproject thread-load "0.3.3"
   :description "Load balancing work units over multiple threads, meant for long running threads. "
   :url "https://github.com/gerritjvv/thread-load"
   :license {:name "Eclipse Public License"
